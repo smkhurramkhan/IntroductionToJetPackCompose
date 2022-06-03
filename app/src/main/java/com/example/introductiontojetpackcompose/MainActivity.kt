ed367d0886145926edf92fedeeddd65689241143
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-06-03 12:53:08 +0500

    splash changed rows and columns added modifiers added

--- app/src/main/java/com/example/introductiontojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/introductiontojetpackcompose/MainActivity.kt
@@ -3,41 +3,155 @@ package com.example.introductiontojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.introductiontojetpackcompose.ui.theme.IntroductionToJetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            IntroductionToJetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(Color.Transparent),
+                    horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                            .border(2.dp, Color.Cyan)
+                            .padding(4.dp)
+                            .background(Color.Red),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Greeting(name = "This")
+                        Greeting(name = "is")
+                        Greeting(name = "first")
+                        Greeting(name = "column")
+                    }
+
+                    Column(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                            .border(2.dp, Color.Cyan)
+                            .padding(4.dp)
+                            .background(Color.Green),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Greeting(name = "This")
+                        Greeting(name = "is")
+                        Greeting(name = "Second")
+                        Greeting(name = "column")
+                    }
+                }
+
+
+
+                Row(
+                    modifier = Modifier
+                        .border(2.dp, Color.Magenta)
+                        .padding(8.dp)
+                        .background(Color.Transparent),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                            .padding(4.dp)
+                            .background(Color.Yellow),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Greeting(name = "This")
+                        Greeting(name = "is")
+                        Greeting(name = "second")
+                        Greeting(name = "row")
+                        Greeting(name = "first")
+                        Greeting(name = "column")
+                    }
+
+                    Column(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                            .padding(4.dp)
+                            .background(Color.Blue),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Greeting(name = "This")
+                        Greeting(name = "is")
+                        Greeting(name = "second")
+                        Greeting(name = "row")
+                        Greeting(name = "second")
+                        Greeting(name = "column")
+                    }
                 }
             }
+
         }
+
     }
 }
 
+
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "$name")
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntroductionToJetPackComposeTheme {
-        Greeting("Android")
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Green),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Greeting(name = "This")
+            Greeting(name = "is")
+            Greeting(name = "a")
+            Greeting(name = "Row")
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Green),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Greeting(name = "This")
+            Greeting(name = "is")
+            Greeting(name = "a")
+            Greeting(name = "column")
+        }
+
     }
 }
--- app/src/main/java/com/example/introductiontojetpackcompose/SplashActivity.kt
+++ app/src/main/java/com/example/introductiontojetpackcompose/SplashActivity.kt
@@ -1,77 +1,60 @@
 package com.example.introductiontojetpackcompose
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.runtime.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import com.example.introductiontojetpackcompose.navigation.Screen
-import com.example.introductiontojetpackcompose.ui.theme.Purple700
-import kotlinx.coroutines.delay
 
-@Composable
-fun SplashActivity(navController: NavHostController) {
-    var startAnimation by remember { mutableStateOf(false) }
-    val alphaAnim = animateFloatAsState(
-        targetValue = if (startAnimation) 1f else 0f,
-        animationSpec = tween(
-            durationMillis = 3000
-        )
-    )
 
-    LaunchedEffect(key1 = true) {
-        startAnimation = true
-        delay(4000)
-        navController.popBackStack()
-        navController.navigate(Screen.Home.route)
-    }
-    Splash(alpha = alphaAnim.value)
-}
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(
 
-@Composable
-fun Splash(alpha: Float) {
-    Box(
-        modifier = Modifier
-            .background(
-                if (isSystemInDarkTheme()) Color.Black
-                else Purple700
-            )
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Icon(
-            modifier = Modifier
-                .size(120.dp)
-                .alpha(alpha = alpha),
-            imageVector = Icons.Default.Email,
-            contentDescription = "Logo Icon",
-            tint = Color.White
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Red),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    splashName(name = getString(R.string.app_name))
+
+                }
+
+            }
+
+
+        }
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+            }, 3000
         )
     }
 }
 
 @Composable
-@Preview
-fun SplashScreenPreview() {
-    Splash(alpha = 1f)
+fun splashName(name: String) {
+    Text(text = "$name!")
 }
 
-@Composable
-@Preview(uiMode = UI_MODE_NIGHT_YES)
-fun SplashScreenDarkPreview() {
-    Splash(alpha = 1f)
-}
+
+
