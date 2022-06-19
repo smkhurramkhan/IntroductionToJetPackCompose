21c3656e39aac5df5ac8104e9ccd2fbde2eb0308
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-06-19 23:15:46 +0500

    working on fulll layout activity

--- app/src/main/java/com/example/introductiontojetpackcompose/ui/FullLayoutActivity.kt
+++ app/src/main/java/com/example/introductiontojetpackcompose/ui/FullLayoutActivity.kt
@@ -3,41 +3,59 @@ package com.example.introductiontojetpackcompose.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.introductiontojetpackcompose.ui.ui.theme.IntroductionToJetPackComposeTheme
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class FullLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            IntroductionToJetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState = remember {
+                mutableStateOf("")
+            }
+            val scope = rememberCoroutineScope()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
                 ) {
-                    Greeting2("Android")
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text(text = "Enter Your Name")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                        }
+                    }) {
+                        Text(text = "Please Greet me")
+                    }
                 }
             }
+
         }
-    }
-}
-
-@Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    IntroductionToJetPackComposeTheme {
-        Greeting2("Android")
     }
 }
