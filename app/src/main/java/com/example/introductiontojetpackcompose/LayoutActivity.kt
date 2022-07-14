8de2a6f0e9bfd9e54eb31693b42cf1c7e6da8681
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-07-14 16:52:20 +0500

    worked on simple animations spring, tweens and keyframes

--- app/src/main/java/com/example/introductiontojetpackcompose/LayoutActivity.kt
+++ app/src/main/java/com/example/introductiontojetpackcompose/LayoutActivity.kt
@@ -3,6 +3,7 @@ package com.example.introductiontojetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -29,17 +30,23 @@ class LayoutActivity : ComponentActivity() {
 
                 val size by animateDpAsState(
                     targetValue = sizeState,
-                   keyframes {
-                       durationMillis = 5000
-                       sizeState at 0 with LinearEasing
-                       sizeState*1.5f at 1000 with FastOutLinearInEasing
-                       sizeState*2 at 5000
-                   }
+                   tween(durationMillis = 1000)
                 )
+
+                val infiniteTransition = rememberInfiniteTransition()
+                val color by infiniteTransition.animateColor(
+                    initialValue = Color.Red,
+                    targetValue = Color.Green ,
+                    animationSpec = infiniteRepeatable(
+                        tween(durationMillis = 2000),
+                        repeatMode = RepeatMode.Reverse
+                    )
+                )
+
                 Box(
                     modifier = Modifier
                         .size(size)
-                        .background(Color.Red),
+                        .background(color),
                     contentAlignment = Alignment.Center
                 ) {
                     Button(onClick = {
