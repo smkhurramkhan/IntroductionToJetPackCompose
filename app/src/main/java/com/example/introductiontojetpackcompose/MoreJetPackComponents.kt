40d20e0c56140733e04defda603671f66518a9bb
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-08-08 01:09:45 +0500

    working on more jetpack components

---
+++ app/src/main/java/com/example/introductiontojetpackcompose/MoreJetPackComponents.kt
@@ -0,0 +1,11 @@
+package com.example.introductiontojetpackcompose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoreJetPackComponents : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_jet_pack_components)
+    }
+}
