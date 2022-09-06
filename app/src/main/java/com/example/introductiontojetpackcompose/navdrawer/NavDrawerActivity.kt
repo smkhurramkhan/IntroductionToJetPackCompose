95593c9344d04175dd84aacbe40a42eb347529e5
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-09-06 11:58:24 +0500

    started working on nav drawer

---
+++ app/src/main/java/com/example/introductiontojetpackcompose/navdrawer/NavDrawerActivity.kt
@@ -0,0 +1,14 @@
+package com.example.introductiontojetpackcompose.navdrawer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class NavDrawerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
